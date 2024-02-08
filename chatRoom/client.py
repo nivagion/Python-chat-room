3594edb8f11b35b45c887b0001169ca667a0a998
nivagion <cucakleo@gmail.com>
2024-02-08 18:59:03 +0100

    maknut client number i dodano da client upisuje svoje ime i da se ne ispisuje poruka clientu koji ju salje

--- chatRoom/client.py
+++ chatRoom/client.py
@@ -4,15 +4,20 @@ import threading
 def receive_messages(sock):
     while True:
         msg = sock.recv(1024).decode('utf-8')
-        print(f'Received from {msg}')
+        username, _, text = msg.partition(':')
+        if username.strip() != my_username:#ako je ime sa whitespaceom ispred onda usporeduje kao isto, npr " ivan" i "ivan" je isti string
+            print(f'{msg}')
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('5.tcp.eu.ngrok.io', 16755))#u ovom formatu treba ngrok
-#client.connect(('localhost', 12345))#za hosta
+#client.connect(('7.tcp.eu.ngrok.io', 14024))#u ovom formatu treba ngrok
+client.connect(('localhost', 12345))#za hosta
 
-receive_handler = threading.Thread(target=receive_messages, args=(client,))
+my_username = input('Enter username: ')
+client.send(my_username.encode('utf-8'))#šalje username
+
+receive_handler = threading.Thread(target=receive_messages, args=(client,))#threada da moze primat poruke dok pises i saljes svoje
 receive_handler.start()
 
 while True:
     msg = input('')
-    client.send(msg.encode('utf-8'))
+    client.send(msg.encode('utf-8'))#šalje poruku
--- chatRoom/server.py
+++ chatRoom/server.py
@@ -1,23 +1,24 @@
 import socket
 import threading
 
-def handle_client(client_socket, client_number):
+def handle_client(client_socket):
+    username = client_socket.recv(1024).decode('utf-8')#prvo inputa ime koje ce kasnije displayat
     while True:
-        msg = client_socket.recv(1024).decode('utf-8')
-        print(f'{client_number}: {msg}')
-        for client in clients:
-            client.send(f'{client_number}: {msg}'.encode('utf-8'))
+        msg = client_socket.recv(1024).decode('utf-8')#dobiva msg od tog klijenta na threadu
+        print(f'{username}: {msg}')
+        for client in clients:#salje se svima
+            client.send(f'{username}: {msg}'.encode('utf-8'))
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 12345))
 server.listen(5)
 
 clients = []
-client_number = 0
+client_number = 0#ne koristi se vise
 while True:
     client_socket, addr = server.accept()
     clients.append(client_socket)
     client_number += 1
     print(f'Accepted connection from {addr[0]}:{addr[1]}')
-    client_handler = threading.Thread(target=handle_client, args=(client_socket, client_number))
+    client_handler = threading.Thread(target=handle_client, args=(client_socket,))#stvara thread za novog clienta
     client_handler.start()
